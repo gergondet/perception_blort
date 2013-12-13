ad1a2f65f21f4e9973abbb0cc03c192c9e805521
Pierre Gergondet <pierre.gergondet@aist.go.jp>
2013-12-13 18:47:13 +0900

    Add a call to switch tracking on/off for certain objects

--- blort_ros/src/gltracker.h
+++ blort_ros/src/gltracker.h
@@ -94,6 +94,7 @@ namespace blort_ros
         std::vector<Tracking::movement_state> movements;
         std::vector<Tracking::quality_state> qualities;
         std::vector<Tracking::confidence_state> tracking_confidences;
+        std::vector<bool> tracking_objects;
 
         // Protection mutex for multi-threaded access to the model/poses
         boost::mutex models_mutex;
@@ -158,6 +159,8 @@ namespace blort_ros
         virtual void switchToTracking(size_t id);
 
         virtual void switchToRecovery(size_t id);
+
+        virtual void switchTracking(const std::vector<uint8_t> & params);
 
         ~GLTracker();
 
