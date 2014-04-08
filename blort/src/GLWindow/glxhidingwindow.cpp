6718db0308646f0b579f2dab1274ba5c6ead1456
Pierre Gergondet <pierre.gergondet@aist.go.jp>
2014-04-08 16:39:11 +0900

    Improve module_glwindow
    - Hide implementation details into the cpp
    
    This avoids the need to define LINUX to have the proper structure when
    including GLWindow from another project (which can lead to unexpected
    behaviors)

--- blort/src/GLWindow/glxhidingwindow.cpp
+++ blort/src/GLWindow/glxhidingwindow.cpp
@@ -6,24 +6,14 @@
 pal_blort::GLXHidingWindow::GLXHidingWindow()
         : ::blortGLWindow::GLWindow(1, 1, "tracker", false)
 {
-#ifndef WIN32
-    XIconifyWindow(dpy, glWin, 0);
-#endif
 }
 
 pal_blort::GLXHidingWindow::GLXHidingWindow(unsigned int width, unsigned int height)
     : ::blortGLWindow::GLWindow(width, height, "tracker", false)
 {
-#ifndef WIN32
-    XIconifyWindow(dpy, glWin, 0);
-#endif
 }
 
 pal_blort::GLXHidingWindow::GLXHidingWindow(unsigned int width, unsigned int height, const char* name)
     : ::blortGLWindow::GLWindow(width, height, name, false)
 {
-#ifndef WIN32
-  //XIconifyWindow(dpy, glWin, 0);
-  XUnmapWindow(dpy, glWin);
-#endif
 }
